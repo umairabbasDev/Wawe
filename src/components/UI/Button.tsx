5694e795b06a4b6a56dc315a1dfef0a8c70c2f06
umair abbas <umairabbas391@gmail.com>
2023-07-28 22:47:34 +0500

    added login page

---
+++ src/components/UI/Button.tsx
@@ -0,0 +1,5 @@
+const Button = ({ children }: { children: string }) => {
+  return <button>{children}</button>;
+};
+
+export default Button;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import { Button } from "../components";
+
+const Login = () => {
+  return (
+    <div>
+      <h3>Login</h3>
+      <Button>login</Button>
+    </div>
+  );
+};
+
+export default Login;
