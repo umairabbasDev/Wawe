92d04822ee250a47f693636f19ede11f3903ac39
umair abbas <umairabbas391@gmail.com>
2023-07-31 15:14:08 +0500

    adding navigation

--- src/components/UI/NavBar.tsx
+++ src/components/UI/NavBar.tsx
@@ -1,14 +1,50 @@
+import { Box, Typography } from "@mui/joy";
 import { Link } from "react-router-dom";
+import ColorSchemeToggle from "./ThemeToggle";
 
 const NavBar = () => {
   return (
     <nav style={{ margin: 10 }}>
-      <Link to="/" style={{ padding: 5 }}>
-        Home
-      </Link>
-      <Link to="/about" style={{ padding: 5 }}>
-        About
-      </Link>
+      <Box
+        component="header"
+        sx={{
+          py: 1,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography
+          fontWeight="lg"
+          startDecorator={
+            <Box
+              component="span"
+              sx={{
+                width: 24,
+                height: 24,
+                background: (theme) =>
+                  `linear-gradient(45deg, ${theme.vars.palette.primary.solidBg}, ${theme.vars.palette.primary.solidBg} 30%, ${theme.vars.palette.primary.softBg})`,
+                borderRadius: "50%",
+                boxShadow: (theme) => theme.shadow.md,
+                "--joy-shadowChannel": (theme) =>
+                  theme.vars.palette.primary.mainChannel,
+              }}
+            />
+          }
+        >
+          Logo
+          <Box>
+            <Link to="/" style={{ padding: 5 }}>
+              Home
+            </Link>
+            <Link to="/about" style={{ padding: 5 }}>
+              About
+            </Link>
+          </Box>
+        </Typography>
+
+        <ColorSchemeToggle />
+      </Box>
     </nav>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,86 @@
-import { useContext } from "react";
-import { AuthContext } from "../context/Auth";
+// import { useContext } from "react";
+// import { AuthContext } from "../context/Auth";
+import { Box, Button, Typography } from "@mui/joy";
 
 const Home = () => {
-  const { userId, handleLogOut } = useContext(AuthContext);
+  // const { userId, handleLogOut } = useContext(AuthContext);
 
   return (
-    <section>
-      <h5>
-        Your ID is: <span>{userId}</span>
-      </h5>
-      <button className="btn-logout" onClick={handleLogOut}>
-        Log out
-      </button>
-    </section>
+    <Box
+      sx={(theme) => ({
+        width:
+          "clamp(100vw , (var(--Collapsed-breakpoint) - 100vw) * 999, 100vw)",
+        transition: "width var(--Transition-duration)",
+        transitionDelay: "calc(var(--Transition-duration) + 0.1s)",
+        position: "relative",
+        zIndex: 1,
+        display: "flex",
+        [theme.getColorSchemeSelector("dark")]: {
+          backgroundColor: "rgba(19 19 24 / 0.4)",
+        },
+      })}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100dvh",
+          width:
+            "clamp(var(--Form-maxWidth), (var(--Collapsed-breakpoint) - 100vw) * 999, 100%)",
+          maxWidth: "100%",
+          px: 2,
+        }}
+      >
+        <Box
+          component="main"
+          sx={{
+            my: "auto",
+            py: 2,
+            pb: 5,
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            width: 400,
+            maxWidth: "100%",
+            mx: "auto",
+            borderRadius: "sm",
+            "& form": {
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+            },
+          }}
+        >
+          <div>
+            {/* <Typography component="h1" fontSize="xl2" fontWeight="lg">
+              Sign in to your account
+            </Typography> */}
+            <Typography level="body2" sx={{ my: 1, mb: 3 }}>
+              Welcome back
+            </Typography>
+          </div>
+
+          <Button type="submit" fullWidth>
+            Click here
+          </Button>
+        </Box>
+        <Box component="footer" sx={{ py: 3 }}>
+          <Typography level="body3" textAlign="center">
+            © Wawe {new Date().getFullYear()}
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
 export default Home;
+
+// <section>
+//   <h5>
+//     Your ID is: <span>{userId}</span>
+//   </h5>
+//   <button className="btn-logout" onClick={handleLogOut}>
+//     Log out
+//   </button>
+// </section>
