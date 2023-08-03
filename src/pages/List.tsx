be03c268a225c524b44fbf7684e5e958a0d50e8a
umair abbas <umairabbas391@gmail.com>
2023-08-04 03:08:07 +0500

    added fixed button

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Box, Button } from "@mui/joy";
 import { BookCard, MainFooter, ModalFarm } from "../components";
 import { AddBook } from "../components/ListPage";
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
 
 const List = () => {
   const [open, setOpen] = useState<boolean>(false);
@@ -22,17 +23,35 @@ const List = () => {
           display: "flex",
           flexDirection: "row",
           justifyContent: "center",
-
+          my: "2rem",
           gap: 2,
           flexWrap: "wrap",
         }}
       >
-        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map(() => (
+        {[
+          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1,
+          1, 1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        ].map(() => (
           <BookCard />
         ))}
       </Box>
-      <Button variant="outlined" color="neutral" onClick={() => setOpen(true)}>
-        Open modal
+      <Button
+        color="primary"
+        onClick={() => setOpen(true)}
+        size="md"
+        variant="solid"
+        sx={{
+          width: "80px",
+          height: "80px",
+          borderRadius: "50%",
+          zIndex: "100",
+          position: "fixed",
+          bottom: "30px",
+          right: "30px",
+          textAlign: "center",
+        }}
+      >
+        <AddRoundedIcon sx={{ fontSize: 40 }} />
       </Button>
       <MainFooter />
 
