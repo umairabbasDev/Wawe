78937a72df74f67648e01e2c5e639f1c37bc6766
umairabbasDev <umairabbas391@gmail.com>
2024-08-24 08:48:47 +0500

    env added

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,13 @@ import 'firebase/compat/storage';
 import { toast } from 'react-toastify';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBGOM4gzD-yCfskEERNf8FGL4LryKt6ks4",
-  authDomain: "firegram-t.firebaseapp.com",
-  databaseURL: "https://firegram-t.firebaseio.com",
-  projectId: "firegram-t",
-  storageBucket: "firegram-t.appspot.com",
-  messagingSenderId: "1051894698884",
-  appId: "1:1051894698884:web:0ae01370484cc172ec43d1",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
